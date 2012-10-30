532e35adfdcb525014f7bff994377bd2227a2de5
Mansard <nmansard@laas.fr>
2012-10-30 12:33:04 +0100

    Modif the output of test search.

--- unittests/test_search.m
+++ unittests/test_search.m
@@ -1,23 +1,19 @@
 addpath('unittests/utils');
 
-seed=round(rand*10000);
-%seed = 283;8394
-%seed = 5123;
-%seed = 8394;
+seed=round(rand*1000);
 
 [ nh p m r ] = randstackref(seed);
 blabla({'seed = ',seed});
 blabla({'size = ',nh,' x',p});
 [ A b btype nh p mref rref Au bu ] = randstack(nh,p,m,r);
-%[aset abound] = randaset(btype);
 [aset abound] = dummybound(zeros(p,1));
 
 [primal dual h Y ]=active_search_verbose(A,b,btype,aset,abound);
 
-blabla({'Primal optimum = \n',primal'});
-blabla({'\nOptimum active set =\n',dispaset(h)});
+blabla({'\n','Primal optimum = ','\n',primal'});
+blabla({'\n','Optimum active set =','\n',dispaset(h)});
 
-%hdist(primal,h)
+hdist(primal,h)
 
     
 
