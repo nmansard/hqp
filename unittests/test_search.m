7ae04229a8d0317a95430ebd5bd53f9b62dd5bf2
Mansard <nmansard@laas.fr>
2012-10-30 15:17:50 +0100

    Remove the trace lines on the active search.

--- unittests/test_search.m
+++ unittests/test_search.m
@@ -1,6 +1,6 @@
 addpath('unittests/utils');
 
-%seed=round(rand*1000);
+seed=round(rand*10000);
 
 [ nh p m r ] = randstackref(seed);
 blabla({'seed = ',seed});
@@ -8,12 +8,12 @@ blabla({'size = ',nh,' x',p});
 [ A b btype nh p mref rref Au bu ] = randstack(nh,p,m,r);
 [aset abound] = dummybound(zeros(p,1));
 
-%[primal dual h Y ]=active_search_verbose(A,b,btype,aset,abound);
-[primal dual h Y ]=active_search(A,b,btype,aset,abound);
+[primal dual h Y ]=active_search_verbose(A,b,btype,aset,abound);
 
 blabla({'\n','Primal optimum = ','\n',primal'});
 blabla({'\n','Optimum active set =','\n',dispaset(h)});
 
+% Display a more complex diagnostic about the solution.
 %hdist(primal,h)
 
     
